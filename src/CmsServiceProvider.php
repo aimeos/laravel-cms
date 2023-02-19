31f2b6310d4f56aed946ed6d72724c85e1f2f242
Aimeos <aimeos@aimeos.org>
2023-02-19 16:14:09 +0100

    Initial package setup

---
+++ src/CmsServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * @license MIT, http://opensource.org/licenses/MIT
+ */
+
+
+namespace Aimeos\Cms;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Route;
+
+
+class CmsServiceProvider extends ServiceProvider
+{
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 */
+	protected bool $defer = false;
+
+
+	/**
+	 * Bootstrap the application events.
+	 */
+	public function boot()
+	{
+		$this->publishes( [dirname( __DIR__ ) . '/config/cms.php' => config_path( 'cms.php' )], 'config' );
+	}
+
+
+	/**
+	 * Register the service provider.
+	 */
+	public function register()
+	{
+	}
+}

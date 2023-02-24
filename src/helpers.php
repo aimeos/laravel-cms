bc87b9c68dbaecc1c36bc4635d128e5b7bed9310
Aimeos <aimeos@aimeos.org>
2023-02-24 14:06:30 +0100

    Added cmsimage() helper and improved cmsurl()

--- src/helpers.php
+++ src/helpers.php
@@ -5,10 +5,29 @@
  */
 
 
+if( !function_exists( 'cmsimage' ) )
+{
+    function cmsimage( array $data ): string
+    {
+        $list = [];
+
+        foreach( $data as $width => $path ) {
+            $list[] = cmsurl( $path ) . ' ' . $width . 'w';
+        }
+
+        return implode( ',', $list );
+    }
+}
+
+
 if( !function_exists( 'cmsurl' ) )
 {
     function cmsurl( string $path ): string
     {
+        if( \Illuminate\Support\Str::startsWith( $path, ['data:', 'http:', 'https:'] ) ) {
+            return $path;
+        }
+
         return \Illuminate\Support\Facades\Storage::disk( config( 'cms.disk', 'public' ) )->url( $path );
     }
 }

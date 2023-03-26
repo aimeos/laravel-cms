91543e5909ad83f55b8f2d1b59a17bd1bf7982a1
Aimeos <aimeos@aimeos.org>
2023-03-26 17:07:29 +0200

    Moved routes to main.js and made GraphQL URL configurable

--- admin/src/main.js
+++ admin/src/main.js
@@ -1,20 +1,41 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import { ObserveVisibility } from 'vue-observe-visibility'
+import { createRouter, createWebHistory } from 'vue-router'
 import { ApolloClient, InMemoryCache } from '@apollo/client/core'
 import { createApolloProvider } from '@vue/apollo-option'
-
-import '@mdi/font/css/materialdesignicons.css'
-import 'vuetify/styles'
-import './assets/base.css'
 
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
-import router from './routes'
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+import './assets/base.css'
+
 import App from './App.vue'
 
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: () => import('./views/HomeView.vue')
+    },
+    {
+      path: '/pages',
+      name: 'pages',
+      component: () => import('./views/PagesView.vue')
+    },
+    {
+      path: '/files',
+      name: 'files',
+      component: () => import('./views/FilesView.vue')
+    }
+  ]
+})
 
 const pinia = createPinia();
 const vuetify = createVuetify({
@@ -35,10 +56,11 @@ const vuetify = createVuetify({
   },
 })
 
+const node = document.querySelector('#app');
 const cache = new InMemoryCache()
 const apolloClient = new ApolloClient({
   cache,
-  uri: 'http://localhost:8000/graphql',
+  uri: node && node.dataset && node.dataset.graphql || '/graphql',
   credentials: 'include'
 })
 const apolloProvider = createApolloProvider({

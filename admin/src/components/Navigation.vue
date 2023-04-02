da7c6f8dcf6195f41f172f51df8996ed9637e094
Aimeos <aimeos@aimeos.org>
2023-04-02 15:57:41 +0200

    Added navigation component

---
+++ admin/src/components/Navigation.vue
@@ -0,0 +1,26 @@
+<script>
+  export default {
+    props: ['state'],
+    emits: ['update:state'],
+    data: () => ({
+      width: window.innerWidth
+    })
+  }
+</script>
+
+<template>
+  <v-navigation-drawer width="220" :modelValue="state" @update:modelValue="$emit('update:state', $event)" :rail="width > 1200 ? false : true" expand-on-hover>
+    <v-list>
+      <v-list-item prepend-icon="mdi-file-tree" title="Pages" value="pages"></v-list-item>
+    </v-list>
+
+    <v-divider></v-divider>
+
+    <v-list>
+      <v-list-item prepend-icon="mdi-folder-multiple-image" title="Files" value="files"></v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<style scoped>
+</style>

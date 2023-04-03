db14747ca3d65e96ab149672214a9f52a2e98e84
Aimeos <aimeos@aimeos.org>
2023-04-03 12:56:41 +0200

    Added global app store

--- admin/src/stores.js
+++ admin/src/stores.js
@@ -1,21 +1,31 @@
 import { defineStore } from 'pinia'
 
+const app = document.querySelector('#app');
+
+export const useAppStore = defineStore('app', {
+  state: () => {
+    return {
+      me: null,
+      url: app?.dataset.urltemplate || '/:slug/:lang'
+    }
+  }
+})
+
+export const useLanguageStore = defineStore('languages', {
+  state: () => {
+    return {
+      available: JSON.parse(app?.dataset.languages || '{}'),
+      current: app?.dataset.language || 'en'
+    }
+  }
+})
+
 export const useTypesStore = defineStore('types', {
   state: () => {
     return {
       state: null,
       type: null,
       used: [],
-    }
-  }
-})
-
-const app = document.querySelector('#app');
-export const useLanguageStore = defineStore('languages', {
-  state: () => {
-    return {
-      available: JSON.parse(app && app.dataset.languages || '{}'),
-      current: app && app.dataset.language || 'en'
     }
   }
 })

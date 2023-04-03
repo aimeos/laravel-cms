8982a668b4ff52765ffb483b8d59ca47576c2f14
Aimeos <aimeos@aimeos.org>
2023-04-03 12:31:51 +0200

    Use picture element instead of image for cms::image template

--- views/image.blade.php
+++ views/image.blade.php
@@ -1,8 +1,16 @@
-<div class="cms-image {{ ( $main ?? false ) ? 'cms-image-main' : '' }}" itemscope itemprop="image" itemtype="http://schema.org/ImageObject">
+<picture class="cms-picture {{ ( $main ?? false ) ? 'cms-image-main' : '' }}" itemscope itemprop="image" itemtype="http://schema.org/ImageObject">
 	<meta itemprop="representativeOfPage" content="{{ ( $main ?? false ) ? 'true' : 'false' }}">
+
+@foreach( $data['previews'] ?? [] as $preview )
+	<source srcset="<?= cmsurl( $preview['path'] ) ?>"
+		media="(min-width: <?= round( $preview['width'] / 16 ) ?>em)"
+		height="<?= $preview['height'] ?? '' ?>"
+		width="<?= $preview['width'] ?? '' ?>">
+@endforeach
+
 	<img class="img-fluid" itemprop="contentUrl" {{ ( $main ?? false ) ? '' : 'loading="lazy"' }}
-		src="{{ cmsurl( current( $data['previews'] ?? [] ) ) }}"
-		srcset="{{ cmsimage( $data['previews'] ?? [] ) }}"
-		sizes="{{ $sizes ?? '100vw' }}"
+		src="{{ cmsurl( current( $data['previews'] ?? [] ) ?: '' ) }}"
+		height="<?= $preview['height'] ?? '' ?>"
+		width="<?= $preview['width'] ?? '' ?>"
 		alt="{{ $data['name'] ?? '' }}">
-</div>
+</picture>

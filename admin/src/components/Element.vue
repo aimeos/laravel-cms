5d7e1a2701b09aaf99489cabdc1bcbf15cac1d3f
Aimeos <aimeos@aimeos.org>
2023-04-14 16:50:36 +0200

    Added element component stub

---
+++ admin/src/components/Element.vue
@@ -0,0 +1,21 @@
+<script>
+  export default {
+    props: ['data'],
+    emits: ['update:data'],
+    data: () => ({
+    })
+  }
+</script>
+
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="12" md="6">
+        <v-text-field label="Text" variant="underlined"></v-text-field>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<style scoped>
+</style>

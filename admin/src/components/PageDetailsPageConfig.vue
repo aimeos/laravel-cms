535b3d7c3fb906cad2777ff3a5d90dc2102bdaf3
Aimeos <aimeos@aimeos.org>
2025-04-12 13:38:55 +0200

    Implemented page config elements

--- admin/src/components/PageDetailsPageConfig.vue
+++ admin/src/components/PageDetailsPageConfig.vue
@@ -1,48 +1,100 @@
 <script>
+  import Elements from './Elements.vue'
+  import Fields from './Fields.vue'
+
   export default {
+    components: {
+      Elements,
+      Fields,
+    },
     props: ['item'],
     emits: ['update:item'],
     data: () => ({
-      panel: [0],
+      panel: [],
+      config: {},
+      velements: false,
       elements: {
-        'cms::test': {type: 'cms::test', fields: {
-          'test/key': {type: 'cms::string', label: 'Test string config'},
-          'test/color': {type: 'cms::color', label: 'Test color selector'}
-        }},
-        'cms::test2': {type: 'cms::test2', fields: {
-          'test2/key': {type: 'cms::string', label: 'Second string value'}
-        }},
+        'test': {
+          type: 'test',
+          label: 'Test string+color',
+          fields: {
+            'test/key': {type: 'string', label: 'Test string config'},
+            'test/color': {type: 'color', label: 'Test color selector'}
+          }
+        },
+        'test2': {
+          type: 'test2',
+          fields: {
+            'test2/key': {type: 'string', label: 'Second string value'}
+          }
+        },
       }
     }),
+    computed: {
+      available() {
+        return Object.keys(this.elements).length
+      }
+    },
+    methods: {
+      add(code) {
+        if(!this.elements[code]) {
+          console.error(`Element not found "${code}"`)
+          return
+        }
+
+        if(this.config[code]) {
+          return
+        }
+
+        this.config[code] = Object.assign(JSON.parse(JSON.stringify(this.elements[code])), {data: {}})
+        this.panel.push(Object.keys(this.meta).length - 1)
+        this.velements = false
+      },
+
+      remove(code) {
+        delete this.config[code]
+      },
+
+      title(el) {
+        return Object.values(el.data || {}).filter(v => typeof v !== 'object' && !!v).join(' - ').substring(0, 50) || el.label || ''
+      },
+
+      use(data) {
+        this.config = data
+        this.vhistory = null
+      }
+    },
   }
 </script>
 
 <template>
-  <v-container>
-    <v-expansion-panels v-model="panel">
-      <v-expansion-panel v-for="(entry, code) in elements" :key="code" elevation="1">
-        <v-expansion-panel-title>
-          {{ code }}
+  <v-sheet>
+    <v-expansion-panels class="list" v-model="panel" multiple>
+
+      <v-expansion-panel v-for="(el, code) in config || {}" :key="code" elevation="1" rounded="lg">
+        <v-expansion-panel-title expand-icon="mdi-pencil">
+          <v-btn icon="mdi-delete" variant="text" @click="remove(code)"></v-btn>
+          <div class="element-title">{{ title(el) }}</div>
+          <div class="element-type">{{ el.type }}</div>
         </v-expansion-panel-title>
         <v-expansion-panel-text>
-
-          <v-container>
-            <v-row>
-              <v-col v-for="(def, key) in entry.fields" cols="12" md="6">
-                <input :is="name(def.type)" :config="def" v-model="item.config[key]" />
-              </v-col>
-            </v-row>
-          </v-container>
-
+          <Fields :fields="el.fields" v-model:data="el.data" />
         </v-expansion-panel-text>
       </v-expansion-panel>
 
     </v-expansion-panels>
-  </v-container>
+
+    <div v-if="available" class="btn-group">
+      <v-btn icon="mdi-view-grid-plus" color="primary" @click="velements = true"></v-btn>
+    </div>
+  </v-sheet>
+
+  <Teleport to="body">
+    <v-dialog v-model="velements" scrollable width="auto">
+      <Elements :ce="elements" @add="add($event.type)" />
+    </v-dialog>
+  </Teleport>
 </template>
 
 <style scoped>
-  .v-expansion-panel:nth-of-type(2n+1) .v-expansion-panel-title {
-    background-color: #ebf4ff;
-  }
 </style>

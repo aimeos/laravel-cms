28c44a295e9a51482185c3536daba026e728ec2a
Aimeos <aimeos@aimeos.org>
2025-04-12 13:33:23 +0200

    Added validation for table field

--- admin/src/fields/cms/Table.vue
+++ admin/src/fields/cms/Table.vue
@@ -2,16 +2,33 @@
   export default {
     props: ['modelValue', 'config'],
     emits: ['update:modelValue'],
+    methods: {
+      validate(value) {
+        let lines = 0
+        let columns = 0
+
+        value.split('\n').forEach(line => {
+          columns += line.split(';').length
+          lines++
+        })
+
+        return Number.isInteger(columns / lines)
+      }
+    }
   }
 </script>
 
 <template>
-  <v-textarea v-if="field.type === 'table'"
+  <v-textarea
     placeholder="val;val;val
 val;val;val"
     :auto-grow="true"
+    :rules="[
+      v => !!v || 'This field is required',
+      v => validate(v) || 'Invalid format'
+    ]"
     :modelValue="modelValue"
-    @update:modelValue="$emit('update:modelValue', $event)"
+    @update:modelValue="$emit('update:modelValue', $event.replace(/[\r\n]+/g, '\n').replace(/^\n+|\n+$/g, ''))"
     variant="underlined"
     density="comfortable"
     clearable

f7f0bb2ddbe0467522f5e7cf76825d147db824d7
Aimeos <aimeos@aimeos.org>
2025-04-13 15:11:58 +0200

    Improved structure of page details sections

--- admin/src/components/PageDetailsPage.vue
+++ admin/src/components/PageDetailsPage.vue
@@ -37,36 +37,38 @@
 <template>
   <v-form @submit.prevent>
     <v-container>
-      <v-tabs class="subtabs" v-model="tab" align-tabs="center" density="compact">
-        <v-tab value="details">Details</v-tab>
-        <v-tab value="meta">Meta</v-tab>
-        <v-tab value="config">Config</v-tab>
-      </v-tabs>
+      <v-sheet>
+        <v-tabs class="subtabs" v-model="tab" align-tabs="center" density="compact">
+          <v-tab value="details">Details</v-tab>
+          <v-tab value="meta">Meta</v-tab>
+          <v-tab value="config">Config</v-tab>
+        </v-tabs>
 
-      <div class="header">
-        <v-btn icon="mdi-history"
-          :class="{hidden: !item.versions?.length}"
-          @click="vhistory = true"
-          variant="outlined"
-          elevation="0"
-        ></v-btn>
-      </div>
+        <div class="header">
+          <v-btn icon="mdi-history"
+            :class="{hidden: !item.versions?.length}"
+            @click="vhistory = true"
+            variant="outlined"
+            elevation="0"
+          ></v-btn>
+        </div>
 
-      <v-window v-model="tab">
+        <v-window v-model="tab">
 
-        <v-window-item value="details">
-          <PageDetailsPageProps :item="item" @update:item="$emit('update:item', $event)" />
-        </v-window-item>
+          <v-window-item value="details">
+            <PageDetailsPageProps :item="item" @update:item="$emit('update:item', $event)" />
+          </v-window-item>
 
-        <v-window-item value="meta">
-          <PageDetailsPageMeta :item="item" @update:item="$emit('update:item', $event)" />
-        </v-window-item>
+          <v-window-item value="meta">
+            <PageDetailsPageMeta :item="item" @update:item="$emit('update:item', $event)" />
+          </v-window-item>
 
-        <v-window-item value="config">
-          <PageDetailsPageConfig :item="item" @update:item="$emit('update:item', $event)" />
-        </v-window-item>
+          <v-window-item value="config">
+            <PageDetailsPageConfig :item="item" @update:item="$emit('update:item', $event)" />
+          </v-window-item>
 
-      </v-window>
+        </v-window>
+      </v-sheet>
     </v-container>
   </v-form>
 
@@ -78,8 +80,13 @@
 </template>
 
 <style scoped>
-  .subtabs {
-    background-color: rgb(var(--v-theme-surface));
+  .v-sheet {
+    padding-top: 0;
+    padding-bottom: 0;
+  }
+
+  .v-window-item {
+    padding: 0 0.25rem;
   }
 
   .header {
--- admin/src/components/PageDetailsPageConfig.vue
+++ admin/src/components/PageDetailsPageConfig.vue
@@ -62,26 +62,24 @@
 </script>
 
 <template>
-  <v-sheet>
-    <v-expansion-panels class="list" v-model="panel" multiple>
+  <v-expansion-panels class="list" v-model="panel" multiple>
 
-      <v-expansion-panel v-for="(el, code) in config || {}" :key="code" elevation="1" rounded="lg">
-        <v-expansion-panel-title expand-icon="mdi-pencil">
-          <v-btn icon="mdi-delete" variant="text" @click="remove(code)"></v-btn>
-          <div class="element-title">{{ title(el) }}</div>
-          <div class="element-type">{{ el.type }}</div>
-        </v-expansion-panel-title>
-        <v-expansion-panel-text>
-          <Fields :fields="el.fields" v-model:data="el.data" />
-        </v-expansion-panel-text>
-      </v-expansion-panel>
+    <v-expansion-panel v-for="(el, code) in config || {}" :key="code" elevation="1" rounded="lg">
+      <v-expansion-panel-title expand-icon="mdi-pencil">
+        <v-btn icon="mdi-delete" variant="text" @click="remove(code)"></v-btn>
+        <div class="element-title">{{ title(el) }}</div>
+        <div class="element-type">{{ el.type }}</div>
+      </v-expansion-panel-title>
+      <v-expansion-panel-text>
+        <Fields :fields="el.fields" v-model:data="el.data" />
+      </v-expansion-panel-text>
+    </v-expansion-panel>
 
-    </v-expansion-panels>
+  </v-expansion-panels>
 
-    <div v-if="available" class="btn-group">
-      <v-btn icon="mdi-view-grid-plus" color="primary" @click="velements = true"></v-btn>
-    </div>
-  </v-sheet>
+  <div v-if="available" class="btn-group">
+    <v-btn icon="mdi-view-grid-plus" color="primary" @click="velements = true"></v-btn>
+  </div>
 
   <Teleport to="body">
     <v-dialog v-model="velements" scrollable width="auto">

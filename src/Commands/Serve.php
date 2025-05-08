dfae1757b1ac37d7cf4d67c774f15f51000f47a7
Aimeos <aimeos@aimeos.org>
2025-05-08 10:25:38 +0200

    Fixed serve command

--- src/Commands/Serve.php
+++ src/Commands/Serve.php
@@ -7,8 +7,12 @@
 
 namespace Aimeos\Cms\Commands;
 
+use Illuminate\Support\Env;
 use Illuminate\Console\Command;
 use Illuminate\Foundation\Console\ServeCommand;
+use Symfony\Component\Console\Input\InputOption;
+
+use function Illuminate\Support\php_binary;
 
 
 class Serve extends ServeCommand
@@ -16,7 +20,7 @@ class Serve extends ServeCommand
     /**
      * Command name
      */
-    protected $signature = 'cms:serve';
+    protected $name = 'cms:serve';
 
     /**
      * Command description
@@ -24,6 +28,11 @@ class Serve extends ServeCommand
     protected $description = 'Serve the application for CMS development';
 
 
+    /**
+     * Get the full server command.
+     *
+     * @return array
+     */
     protected function serverCommand()
     {
         return [
@@ -35,6 +44,22 @@ class Serve extends ServeCommand
             '-d',
             'post_max_size=100M',
             __DIR__.'/../server.php',
+        ];
+    }
+
+
+    /**
+     * Get the console command options.
+     *
+     * @return array
+     */
+    protected function getOptions()
+    {
+        return [
+            ['host', null, InputOption::VALUE_OPTIONAL, 'The host address to serve the application on', Env::get('SERVER_HOST', 'localhost')],
+            ['port', null, InputOption::VALUE_OPTIONAL, 'The port to serve the application on', Env::get('SERVER_PORT')],
+            ['tries', null, InputOption::VALUE_OPTIONAL, 'The max number of ports to attempt to serve from', 10],
+            ['no-reload', null, InputOption::VALUE_NONE, 'Do not reload the development server on .env file changes'],
         ];
     }
 }

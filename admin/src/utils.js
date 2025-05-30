e58635b0333ef38236e667d96208b0be6a9fd6b6
Aimeos <aimeos@aimeos.org>
2025-05-30 22:56:37 +0200

    Improved generating content IDs

--- admin/src/utils.js
+++ admin/src/utils.js
@@ -3,14 +3,23 @@
  *
  * @returns String Unique content ID
  */
-export function contentid() {
+const contentid = (function () {
   const BASE64 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_'
-  const EPOCH = new Date('2025-01-01T00:00:00Z')
-  const PRECISION = 30
+  const EPOCH = new Date('2025-01-01T00:00:00Z').getTime();
 
-  let steps = Math.floor((new Date() - EPOCH) / PRECISION)
+  let counter = 0
 
-  return Array.from({ length: 6 }, (_, i) =>
-    BASE64[(steps >> (5 - i) * 6) & 63]
-  ).join('')
-}
+  return function() {
+    counter = (counter + 1) % 16
+
+    // IDs will repeat after 45.3 years
+    const time = Math.floor(Date.now() - EPOCH) / 333
+    const value = (time << 4) | counter;
+
+    return Array.from({ length: 6 }, (_, i) =>
+      BASE64[(value >> (6 * (5 - i))) & 63]
+    ).join('');
+  }
+})()
+
+export { contentid }

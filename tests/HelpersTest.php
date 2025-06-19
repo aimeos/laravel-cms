334bae7f1e518452173d2d603bc02fe3dce4a3b3
Aimeos <aimeos@aimeos.org>
2025-06-19 16:15:53 +0200

    Fixed and improved helper tests

--- tests/HelpersTest.php
+++ tests/HelpersTest.php
@@ -2,12 +2,31 @@
 
 namespace Tests;
 
+use Database\Seeders\CmsSeeder;
+use Aimeos\Cms\Models\Page;
 
-class HelpersTest extends \Orchestra\Testbench\TestCase
+
+class HelpersTest extends TestAbstract
 {
-	public function testCmsImage()
+	public function testCms()
 	{
-		$this->assertEquals( '/storage/not/exists.jpg 1w', cmsimage( [1 => 'not/exists.jpg'] ) );
+        $this->seed( CmsSeeder::class );
+
+        $page = Page::where('tag', 'root')->firstOrFail();
+
+		$this->assertEquals( '/', cms( $page, 'path' ) );
+	}
+
+
+	public function testCmsAsset()
+	{
+		$this->assertEquals( 'http://localhost/not/exists.js?v=0', cmsasset( 'not/exists.js' ) );
+	}
+
+
+	public function testCmsSrcset()
+	{
+		$this->assertEquals( '/storage/not/exists.jpg 1w', cmssrcset( [1 => 'not/exists.jpg'] ) );
 	}
 
 

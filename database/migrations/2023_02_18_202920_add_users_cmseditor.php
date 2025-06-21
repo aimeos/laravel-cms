966ccc6fe3159d0323edd2d2e4af1706fc01f37b
Aimeos <aimeos@aimeos.org>
2025-06-21 18:39:50 +0200

    Extended permission column in users table

--- database/migrations/2023_02_18_202920_add_users_cmseditor.php
+++ database/migrations/2023_02_18_202920_add_users_cmseditor.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->integer('cmseditor')->default(0);
+            $table->biginteger('cmseditor')->default(0);
         });
     }
 

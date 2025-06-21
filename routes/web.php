2c98eb767d6c756d937b9f0a53b7b6f60f2afcd8
Aimeos <aimeos@aimeos.org>
2025-06-21 18:35:07 +0200

    Fixed missing contact route

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,12 @@ Route::get('cmsadmin/{path?}', [Controllers\PageController::class, 'admin'])
     ->where(['path' => '.*'])
     ->name('cms.admin');
 
+Route::post('cmsapi/contact', [Controllers\ApiController::class, 'contact'])
+    ->middleware('throttle:2,1')
+    ->name('cms.api.contact');
+
 Route::group(config('cms.multidomain') ? ['domain' => '{domain}'] : [], function() {
     Route::get('{path?}', [Controllers\PageController::class, 'index'])
+        ->middleware(['web'])
         ->name('cms.page');
 });

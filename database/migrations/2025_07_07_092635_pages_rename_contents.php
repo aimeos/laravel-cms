ee3f26b453cb62535a4b6e3d6ebd49824772cbb5
Aimeos <aimeos@aimeos.org>
2025-07-07 11:33:34 +0200

    Added migration for next release

---
+++ database/migrations/2025_07_07_092635_pages_rename_contents.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('cms_pages', function (Blueprint $table) {
+            $table->renameColumn('content', 'contents');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('cms_pages', function (Blueprint $table) {
+            $table->renameColumn('contents', 'content');
+        });
+    }
+};

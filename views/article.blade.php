8b9ea9a361dae5b6e7836cc82464643a7b1bb498
Aimeos <aimeos@aimeos.org>
2025-07-10 08:19:09 +0200

    Minor improvements

--- views/article.blade.php
+++ views/article.blade.php
@@ -2,15 +2,11 @@
 <link type="text/css" rel="stylesheet" href="{{ cmsasset('vendor/cms/article.css') }}">
 @endPushOnce
 
-<h1 class="title">{{ cms($page, 'title') }}</h1>
-
 @if($file = $files[@$data->file?->id] ?? null)
 	@include('cms::pic', ['file' => $file, 'main' => true, 'class' => 'cover'])
 @endif
 
-<div class="lead">
-	@markdown(@$data->lead)
-</div>
+<h1 class="title">{{ cms($page, 'title') }}</h1>
 
 <div class="text">
 	@markdown(@$data->text)

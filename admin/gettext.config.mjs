fa407852f8fe4a1faa74ffd5f8501561f7a41e7c
Aimeos <aimeos@aimeos.org>
2025-07-16 21:31:22 +0200

    Added translations

---
+++ admin/gettext.config.mjs
@@ -0,0 +1,12 @@
+export default {
+  output: {
+    path: "./i18n",
+    locales: [
+      'ar', 'bg', 'cs', 'da', 'de', 'el', 'en', 'es', 'et', 'fi', 'fr',
+      'he', 'hu', 'id', 'it', 'ja', 'ko', 'lt', 'lv', 'no', 'nl', 'pl',
+      'pt', 'ro', 'ru', 'sk', 'sl', 'sv', 'th', 'tr', 'uk', 'vi', 'zh'
+    ],
+    jsonPath: "./",
+    splitJson: true,
+  },
+};

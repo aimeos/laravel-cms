1929755e34add75217cc3600c8bb0400fe3f7fa4
Aimeos <aimeos@aimeos.org>
2025-07-19 13:36:53 +0200

    Build dist files

---
+++ admin/dist/assets/LINGUAS_raw-FT26ImPo.js
@@ -0,0 +1,1 @@
+const t="ar bg cs da de el en es et fi fr he hu id it ja ko lt lv no nl pl pt ro ru sk sl sv th tr uk vi zh";export{t as default};
---
+++ admin/dist/assets/en-ZdYmxoc1.js
@@ -0,0 +1,1 @@
+const e={},n={en:e};export{n as default,e as en};

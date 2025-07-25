a138a1670aab89b8a55f09b3a136f9dba96f0a3d
Aimeos <aimeos@aimeos.org>
2025-07-25 23:52:34 +0200

    Extends Pages tool class for language and searching page versions

--- src/Tools/Pages.php
+++ src/Tools/Pages.php
@@ -11,19 +11,27 @@ class Pages extends Tool
     public function __construct()
     {
         $this->as( 'search-pages' )
-            ->for( 'Searches the page tree for pages matching a keyword or phrase. Returns up to 10 matching pages as JSON array of page entries.' )
-            ->withStringParameter( 'term', 'Search keyword, e.g., "blog", "product", or "FAQ". One word or phrase only.')
+            ->for( 'Searches the page tree for pages containing a keyword. Returns up to 10 matching pages as JSON array.' )
+            ->withStringParameter( 'term', 'Search keyword, e.g., "blog", "product", or "FAQ". One word or page path only.')
+            ->withStringParameter( 'lang', 'ISO language code of the pages to search for, e.g. "en" or "en-US".')
             ->using( $this );
     }
 
 
-    public function __invoke( string $term ): string
+    public function __invoke( string $term, string $lang ): string
     {
-        $result = Page::whereAny( ['config', 'content', 'meta', 'name', 'title'], 'like', '%' . $term . '%' )
-            ->orderBy( 'id', 'desc' )
+        $result = Page::withoutTrashed()
+            ->where( function( $builder ) use ( $lang, $term ) {
+                $builder->whereAny( ['content', 'meta', 'name', 'path', 'title'], 'like', '%' . $term . '%' )
+                ->where( 'lang', $lang );
+            } )
+            ->orWhereHas('versions', function( $builder ) use ( $lang, $term  ) {
+                $builder->where( 'data', 'like', '%' . $term . '%' )
+                    ->where( 'lang', $lang );
+            } )
             ->take( 10 )
             ->get()
-            ->map( fn( $item ) => $item->toArray() + ['url' => route('cms.page', ['path' => $item->path])] );
+            ->map( fn( $item ) => $item->toArray() + ['url' => route( 'cms.page', ['path' => $item->path] )] );
 
         return response()->json( $result );
     }
